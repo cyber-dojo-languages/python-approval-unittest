3f646152f9c689e726db8b129f93ac84050f14fb
Jon Jagger <jon@jaggersoft.com>
2020-05-23 16:31:19 +0100

    First working version

--- start_point/test_hiker.py
+++ start_point/test_hiker.py
@@ -2,18 +2,19 @@ import hiker
 import unittest
 
 from approvaltests.approvals import verify
-from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+from approvaltests.reporters.generic_diff_reporter_factory \
+    import GenericDiffReporterFactory
 
 
-class GettingStartedTest(unittest.TestCase):
+class HikerTest(unittest.TestCase):
     def setUp(self):
         self.reporter = GenericDiffReporterFactory().get_first_working()
 
-    def test_life_the_universe_and_everything(self):
-       '''a simple example to start you off'''
-       douglas = hiker.Hiker()
-       result = str(douglas.answer())
-       verify(result, self.reporter)       
+    def test_hhgttg(self):
+        '''a simple example to start you off'''
+        douglas = hiker.Hiker()
+        result = str(douglas.answer())
+        verify(result, self.reporter)
 
 
 if __name__ == "__main__":
